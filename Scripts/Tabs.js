a764a7b84cab1a8a439979445ceedb0523434296
Gowda <SP1YPUTT@adph.state.al.us>
2018-07-13 12:24:20 -0500

    Data table template using Bootstrap and Jquery

---
+++ Scripts/Tabs.js
@@ -0,0 +1,14 @@
+﻿$('#showNPI').click(function () {    
+    $("#allNPIDiv").show();
+    $("#orgDocDiv").hide();
+});
+$('#showOrg').click(function () {
+   $("#allNPIDiv").hide(); 
+    $("#orgDocDiv").show();
+      
+    
+});
+function ResetButtons() {
+    $("button").css("background-color", "#31b0d5");
+
+}
